a58e7125264268d2b33384fc973da6be08a744c5
Kiril <kiril@Mac-jenya.local>
2025-05-09 11:33:30 +0300

    add-new-style-detail-page

--- mars-studio/studio/settings.py
+++ mars-studio/studio/settings.py
@@ -18,7 +18,7 @@ SITE_URL = 'https://www.web-mars.com'
 
 ALLOWED_HOSTS = ['web-mars.com', 'www.web-mars.com', '127.0.0.1', 'localhost']
 
-DEBUG = False
+DEBUG = True
 SECURE_SSL_REDIRECT = True 
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
